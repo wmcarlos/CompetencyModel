5b7ab544d050126deab60268363a7e444834f061
Carlos Vargas <kr-lo-ss@hotmail.com>
2017-06-11 23:05:00 +0200

    update the database structure

--- database/CompetitionModel.sql
+++ database/CompetitionModel.sql
@@ -122,3 +122,95 @@ CREATE TABLE cm_charge_assigned(
 	CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES cm_user (user_id) ON UPDATE CASCADE ON DELETE RESTRICT,
 	CONSTRAINT fk_charge_id FOREIGN KEY (charge_id) REFERENCES cm_charge_id (charge_id) ON UPDATE CASCADE ON DELETE RESTRICT
 )ENGINE = INNODB;
+
+CREATE TABLE cm_domain_level(
+	domain_level_id INT AUTO_INCREMENT NOT NULL,
+	company_id INT NOT NULL,
+	name VARCHAR(60) NOT NULL,
+	scale VARCHAR(5) NOT NULL,
+	position INT NOT NULL DEFAULT 1,
+	value NUMERIC, NOT NULL DEFAULT 0.0,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated TIMESTAMP NULL,
+	isactive CHAR(1) NOT NULL DEFAULT 'Y',
+	CONSTRAINT pk_domain_level_id PRIMARY KEY (domain_level_id),
+	CONSTRAINT fk_company_id04 FOREIGN KEY (comapny_id) REFERENCES cm_company (company_id) ON UPDATE CASCADE ON DELETE RESTRICT
+)ENGINE = INNODB;
+
+CREATE TABLE cm_development_level(
+	development_level_id INT AUTO_INCREMENT NOT NULL,
+	company_id INT NOT NULL,
+	name VARCHAR(60) NOT NULL,
+	position INT NOT NULL DEFAULT 1,
+	value NUMERIC NOT NULL DEFAULT 0.0,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated TIMESTAMP NULL,
+	isactive CHAR(1) NOT NULL DEFAULT 'Y',
+	CONSTRAINT pk_development_level_id PRIMARY KEY (development_level_id),
+	CONSTRAINT fk_company_id06 FOREIGN KEY (comapny_id) REFERENCES cm_company (company_id) ON UPDATE CASCADE ON DELETE RESTRICT
+)ENGINE = INNODB;
+
+CREATE TABLE cm_competency(
+	competency_id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(100) NOT NULL,
+	definition TEXT NULL,
+	company_id INT NOT NULL,
+	charge_level_id INT NOT NULL,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated TIMESTAMP NULL,
+	isactive CHAR(1) NOT NULL DEFAULT 'Y',
+	CONSTRAINT pk_competency_id PRIMARY KEY (competency_id),
+	CONSTRAINT fk_company_id07 FOREIGN KEY (comapny_id) REFERENCES cm_company (company_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT fk_charge_level_id FOREIGN KEY (charge_level_id) REFERENCES cm_charge_level (charge_level_id) ON UPDATE CASCADE ON DELETE RESTRICT
+)ENGINE = INNODB;
+
+CREATE TABLE cm_behavioral_indicator(
+	behavioral_indicator_id INT AUTO_INCREMENT NOT NULL,
+	competency_id INT AUTO_INCREMENT NOT NULL,
+	description TEXT NOT NULL,
+	development_level_id INT NOT NULL,
+	position INT NOT NULL DEFAULT 1,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated TIMESTAMP NULL,
+	isactive CHAR(1) NOT NULL DEFAULT 'Y',
+	CONSTRAINT pk_behavioral_indicator PRIMARY KEY (behavioral_indicator_id),
+	CONSTRAINT fk_competency_id FOREIGN KEY (competency_id) REFERENCES cm_competency (competency_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT fk_development_level_id FOREIGN KEY (development_level_id) REFERENCES cm_development_level (development_level_id) ON UPDATE CASCADE ON DELTE RESTRICT
+)ENGINE = INNODB;
+
+CREATE TABLE cm_instrument_of_evaluation(
+	instrument_of_evaluation_id INT AUTO_INCREMENT NOT NULL,
+	company_id INT NOT NULL,
+	name VARCHAR(150) NOT NULL,
+	description TEXT NULL,
+	instructions TEXT NULL,
+	evaluationtype CHAR(2) NOT NULL DEFAULT 'UC',
+	charge_level_id INT NOT NULL,
+	status CHAR(2) NOT NULL DEFAULT 'DR',
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated TIMESTAMP NULL,
+	isactive CHAR(1) NOT NULL DEFAULT 'Y',
+	CONSTRAINT pk_instrument_of_evaluation_id PRIMARY KEY (instrument_of_evaluation_id),
+	CONSTRAINT fk_company_id08 FOREIGN KEY (comapny_id) REFERENCES cm_company (company_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT fk_charge_level_id01 FOREIGN KEY (charge_level_id) REFERENCES cm_charge_level (charge_level_id) ON UPDATE CASCADE ON DELETE RESTRICT
+)ENGINE = INNODB;
+
+CREATE TABLE cm_ponderation_charge_level(
+	ponderation_charge_level_id INT AUTO_INCREMENT NOT NULL,
+	instrument_of_evaluation_id INT NOT NULL,
+	charge_level_id INT NOT NULL,
+	value NUMERIC NOT NULL DEFAULT 0.0,
+	CONSTRAINT pk_ponderation_charge_level_id PRIMARY KEY (ponderation_charge_level_id),
+	CONSTRAINT fk_instrument_of_evaluation_id FOREIGN KEY (instrument_of_evaluation_id) REFERENCES cm_instrument_of_evaluation (instrument_of_evaluation_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT fk_charge_level_id02 FOREIGN KEY (charge_level_id) REFERENCES cm_charge_level (charge_level_id) ON UPDATE CASCADE ON DELETE RESTRICT
+)ENGINE = INNODB;
+
+CREATE TABLE competency_instrument(
+	competency_instrument_id INT AUTO_INCREMENT NOT NULL,
+	instrument_of_evaluation_id INT NOT NULL,
+	competency_id INT NOT NULL,
+	position INT NOT NULL DEFAULT 1,
+	CONSTRAINT pk_competency_instrument_id PRIMARY KEY (competency_instrument_id),
+	CONSTRAINT fk_instrument_of_evaluation_id01 FOREIGN KEY (instrument_of_evaluation_id) REFERENCES cm_instrument_of_evaluation (instrument_of_evaluation_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT fk_competency_id01 FOREIGN KEY (competency_id) REFERENCES cm_competency (competency_id) ON UPDATE CASCADE ON DELETE RESTRICT
+)ENGINE = INNODB;
