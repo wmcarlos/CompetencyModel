377cb9f7603223b655909555805c9c3c83a01a53
avargas <avargas@frontuari.com>
2017-06-15 23:13:51 -0400

    Agregando icono

--- application/views/admin/template/header.php
+++ application/views/admin/template/header.php
@@ -16,6 +16,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Competency Model | <?= $title ?></title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="SHORTCUT ICON" href="<?= base_url() ?>public/img/icon.fw.png"/>
   <link rel="stylesheet" href="<?= base_url() ?>public/plugins/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>public/plugins/font-awesome-4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>public/plugins/ionicons-2.0.1/css/ionicons.min.css">
