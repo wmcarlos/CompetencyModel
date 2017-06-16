63ac3abf4e6ba89e08e9927074e886e16ae4d6c6
avargas <avargas@frontuari.com>
2017-06-16 15:27:05 -0400

    Agregando libreria transaccional2

--- application/views/admin/competency/create.php
+++ application/views/admin/competency/create.php
@@ -7,17 +7,17 @@
       <div class="box-body">
       <?= form_open_multipart("Competencies/$action",array('autocomplete' => 'off')) ?>
 
- 			<div class='form-group'>
- 				<label for="txtcompany_id">Compa&ntilde;ia:</label>
- 				<select class="form-control textuppercase" name="txtcompany_id" id="txtcompany_id">
+      <div class='form-group'>
+        <label for="txtcompany_id">Compa&ntilde;ia:</label>
+        <select class="form-control textuppercase" name="txtcompany_id" id="txtcompany_id">
             <option value="">Seleccione</option>
             <?= $companies ?>
         </select>
- 			</div>
- 			<div class='form-group'>
- 				<label for="txtname">Nombre:</label>
+      </div>
+      <div class='form-group'>
+        <label for="txtname">Nombre:</label>
         <input type="text" class="form-control textuppercase" name="txtname" id="txtname">
- 			</div>
+      </div>
       <div class='form-group'>
         <label for="txtdefinition">Definicion:</label>
         <textarea class="form-control textuppercase" name="txtdefinition" id="txtdefinition"></textarea>
@@ -76,10 +76,10 @@
           </tr>
         </tbody>
       </table>
- 			<button class="btn btn-success" type="submit"><i class="fa fa-floppy-o" aria-hidden="true"></i> Save</button>
- 			<a class="btn btn-danger" href="<?= base_url() ?>index.php/Competencies"><i class="fa fa-times" aria-hidden="true"></i> Cancel</a>
- 			<button class="btn btn-success" type="submit"><i class="fa fa-floppy-o" aria-hidden="true"></i> Guardar</button>
- 			<a class="btn btn-danger" href="<?= base_url() ?>index.php/Competencies"><i class="fa fa-times" aria-hidden="true"></i> Cancelar</a>
+      <button class="btn btn-success" type="submit"><i class="fa fa-floppy-o" aria-hidden="true"></i> Save</button>
+      <a class="btn btn-danger" href="<?= base_url() ?>index.php/Competencies"><i class="fa fa-times" aria-hidden="true"></i> Cancel</a>
+      <button class="btn btn-success" type="submit"><i class="fa fa-floppy-o" aria-hidden="true"></i> Guardar</button>
+      <a class="btn btn-danger" href="<?= base_url() ?>index.php/Competencies"><i class="fa fa-times" aria-hidden="true"></i> Cancelar</a>
 
       <?= form_close() ?>
       </div>
