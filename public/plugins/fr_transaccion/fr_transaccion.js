592932167309288b81e756258b34d32addd0f288
avargas <avargas@frontuari.com>
2017-06-17 15:33:08 -0400

    Agregando multidetalles en la libreria

--- public/plugins/fr_transaccion/fr_transaccion.js
+++ public/plugins/fr_transaccion/fr_transaccion.js
@@ -87,7 +87,10 @@
 
 		/*funcion para convertir los valores estilo angular en datos y obtener su funcionamiento*/
 		function converter_value_angular(tagbutton){
-			detail_html = $('table#'+tagbutton+' .fr_details').html();
+			
+			//alert("converter angular "+tagbutton);
+			detail_html = $('#'+tagbutton).html();
+			console.log(detail_html);
 		 	piezes_submit = detail_html.split('{{');
 			for(i=0;i<piezes_submit.length;i++){
 				if(piezes_submit[i].indexOf('}}')>-1){
@@ -96,6 +99,7 @@
 				}
 			}
 			//console.log(valores_angular);
+			piezes_submit.length = 0;
 			extract_value_angular(tagbutton);
 		}
 		String.prototype.replaceAll = function(search, replacement) {
@@ -106,54 +110,67 @@
 		function extract_value_angular(tagbutton){
 			/*realizamos el for y vamos a cambiar todo*/
 			for(i=0; i<valores_angular.length;i++){
-
 				idnewtext = $("#"+valores_angular[i]).attr('id');
-				if($("#"+idnewtext).text()){
-					new_text =  $("#"+idnewtext).text();
-					if($("#"+idnewtext).hasClass('fr_select')){
-						new_text = $("#"+idnewtext+" option:selected").text();
+
+				//alert("text "+$("#"+idnewtext).text());
+				if(typeof idnewtext !=='undefined'){
+
+					if($("#"+idnewtext).text()){
+						new_text =  $("#"+idnewtext).text();
+						if($("#"+idnewtext).hasClass('fr_select')){
+							new_text = $("#"+idnewtext+" option:selected").text();
+						}
+					}else{
+						new_text = $("#"+idnewtext).val();
 					}
-				}else{
-					new_text = $("#"+idnewtext).val();
+
 				}
+
 				/*preguntaremos si el select es un hijo*/
-				if(valores_angular[i].indexOf('-value')>-1){
-					array_fr_select = valores_angular[i].split("-");
-					new_text_value = $("#"+array_fr_select[0]).val();
-				}else{
-					new_text_value = '';
+				if(typeof idnewtext !=='undefined'){
+					if(valores_angular[i].indexOf('-value')>-1){
+						array_fr_select = valores_angular[i].split("-");
+						new_text_value = $("#"+array_fr_select[0]).val();
+					}else{
+						new_text_value = '';
+					}
 				}
+
 				/*reemplazamos el texto*/
-				console.log($('table#'+tagbutton+' .fr_details').html());
+				//console.log($("#"+tagbutton).html());
 				//alert(idnewtext);
 				//guardamos el html del tr padre temporalmente
-				tr_padre_temp = $("table#"+tagbutton+" .fr_details tr.tr_padre").html();
+				tr_padre_temp = $("#"+tagbutton+" tr.tr_padre").html();
+
 				//reemplazamos todos los hijos como tal
-				$(".fr_details").html($("table#"+tagbutton+" .fr_details").html().replaceAll('{{'+idnewtext+'}}',new_text));
+				$("#"+tagbutton).html($("#"+tagbutton).html().replaceAll('{{'+idnewtext+'}}',new_text));
 				if(new_text_value!=''){
-					$(".fr_details").html($("table#"+tagbutton+" .fr_details").html().replaceAll('{{'+array_fr_select[0]+'-value}}',new_text_value));
+					$("#"+tagbutton).html($("#"+tagbutton).html().replaceAll('{{'+array_fr_select[0]+'-value}}',new_text_value));
 				}
 				//retornamos nuevamente los valores angular al tr padre
-				$("table#"+tagbutton+" tr.tr_padre").html(tr_padre_temp);
+				$("#"+tagbutton+" tr.tr_padre").html(tr_padre_temp);
 			}
-		
+			/*vaciamos los arreglos*/
+			valores_angular.length=0;
 		}
 
 		function clearfields(){
 			for(i=0; i<valores_angular.length;i++){
 				$("#"+valores_angular[i]).val("");
 			}
+			valores_angular.length = 0;
 			
 		}
 
 
 		/*EVENTOS*/
 		$(document).on('click','.fr_button_add',function(){
-			tagbutton = $(this).attr('tagbutton');
 			/*si la validacion no devolvio nimgun error entra*/
+			tagbutton = $(this).parent().parent().parent().parent().find('.fr_details').attr("id");
+
 			if(fr_validate('button-click')==0){
 				//clonamos el tr padre al hijo
-				$("table#"+tagbutton+" .fr_details").find('.tr_padre').clone().removeClass('tr_padre').addClass('tr_hijo').appendTo('.fr_details');
+				$("#"+tagbutton).find('.tr_padre').clone().removeClass('tr_padre').addClass('tr_hijo').appendTo('#'+tagbutton);
 				converter_value_angular(tagbutton);
 				clearfields();
 			}
