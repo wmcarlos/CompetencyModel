a307045b9f1d56be96f760cfac6d6dc5c0fd4287
Carlos Vargas <kr-lo-ss@hotmail.com>
2017-06-26 21:39:48 +0200

    upload

--- application/views/frontend/template/header.php
+++ application/views/frontend/template/header.php
@@ -9,12 +9,14 @@
   <link rel="stylesheet" href="<?= base_url() ?>public/plugins/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>public/plugins/font-awesome-4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>public/plugins/ionicons-2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>public/plugins/datatables/jquery.dataTables.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>public/css/AdminLTE.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>public/plugins/iCheck/square/blue.css">
   <!-- jQuery 3.1.1 -->
   <script src="<?= base_url() ?>public/plugins/jQuery/jquery-3.1.1.min.js"></script>
   <!-- Bootstrap 3.3.7 -->
   <script src="<?= base_url() ?>public/plugins/bootstrap/js/bootstrap.min.js"></script>
+  <script src="<?= base_url() ?>public/plugins/datatables/jquery.dataTables.min.js"></script>
   <!-- iCheck -->
   <script src="<?= base_url() ?>public/plugins/iCheck/icheck.min.js"></script>
   <script src="<?= base_url() ?>public/js/bootbox.min.js"></script>
--- public/js/global.js
+++ public/js/global.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-    $('.sidebar-menu').tree();
     $(".datatable").DataTable({
     	'language':{
 		    "sProcessing":     "Procesando...",
@@ -27,14 +26,6 @@ $(document).ready(function () {
 		    }
 		}
     });
-
-    /*$(function () {
-	    $('input').iCheck({
-	      checkboxClass: 'icheckbox_square-blue',
-	      radioClass: 'iradio_square-blue',
-	      increaseArea: '20%' // optional
-	    });
-	});*/
     
 });
 
